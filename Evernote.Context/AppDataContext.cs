2468e15b1c63660513a8887388ee5f3b5dfb99a0
EugeneHolovetskiy <ipz201_gev@student.ztu.edu.ua>
2023-01-14 12:27:34 +0200

    Created references in context

--- Evernote.Context/AppDataContext.cs
+++ Evernote.Context/AppDataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Evernote.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace Evernote.Context {
@@ -6,6 +7,46 @@ namespace Evernote.Context {
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Note>(
+                    note => {
+
+
+                        note.HasOne(n => n.User)
+                            .WithMany(u => u.Notes)
+                            .HasForeignKey(n => n.UserId);
+
+
+
+                        note.HasMany(n => n.Tags)
+                            .WithMany(t => t.Notes)
+                            .UsingEntity<TagNote>(
+                                tn => tn.HasOne(p => p.Tag)
+                                    .WithMany(a => a.TagNotes)
+                                    .HasForeignKey(a => a.TagId),
+
+                                tn => tn.HasOne(p => p.Note)
+                                    .WithMany(a => a.TagNotes)
+                                    .HasForeignKey(a => a.NoteId),
+
+                                tn => tn.HasKey(
+                                    qtn => new { qtn.NoteId, qtn.TagId })
+                                );
+
+                    });
+            modelBuilder.Entity<Image>(
+                   img => {
+
+
+                       img.HasOne(n => n.Note)
+                           .WithMany(u => u.Images)
+                           .HasForeignKey(n => n.NoteId);
+
+
+
+                   });
+
+
         }
     }
-    }
+}
